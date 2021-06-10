5f7886b2b5d5a4e16db91ddd44834f5e9aee0b02
Tis Lais <tistististis@gmail.com>
2021-06-10 16:46:43 -0700

    initial commit

---
+++ data/setup.js
@@ -0,0 +1,13 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export default (pool) => {
+  return fs
+    .readFile(
+      `${path.dirname(new URL(import.meta.url).pathname)}/../sql/setup.sql`,
+      {
+        encoding: 'utf-8',
+      }
+    )
+    .then((sql) => pool.query(sql));
+};
