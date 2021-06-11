5cbc2a3ba37665a693fa640c787742bbf2e8eac7
Tis Lais <tistististis@gmail.com>
2021-06-11 16:01:46 -0700

    add passing get by id route model and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Beer from '../lib/models/Beer.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -18,10 +19,20 @@ describe('demo routes', () => {
         color: 'dark'
       });
     expect(res.body).toEqual({
-      id: '1',
+      id: 1,
       name: 'barney',
       abv: '9%',
       color: 'dark'
     });
+  });
+
+  it('finds a beer by id via GET', async () => {
+    const beer = await Beer.insert({
+      name: 'lagunitas',
+      abv: '6.8%',
+      color: 'medium dark'
+    });
+    const res = await request(app).get(`/api/v1/beers/${beer.id}`);
+    expect(res.body).toEqual(beer);
   });
 });
