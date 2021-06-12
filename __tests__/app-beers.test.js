f178bb588047c57c78b3691ad8af1d054d3b986a
Tis Lais <tistististis@gmail.com>
2021-06-11 18:17:11 -0700

    add passing cubes post test

--- __tests__/app-beers.test.js
+++ __tests__/app-beers.test.js
@@ -4,7 +4,7 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Beer from '../lib/models/Beer.js';
 
-describe('demo routes', () => {
+describe('Beer routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
--- __tests__/app-cubes.test.js
+++ __tests__/app-cubes.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Cube from '../lib/models/Cube.js';
+//import Cube from '../lib/models/Cube.js';
 
 describe('Cube routes', () => {
   beforeEach(() => {
@@ -11,16 +11,19 @@ describe('Cube routes', () => {
 
   it('creates a cube via POST', async () => {
     const res = await request(app)
-      .post('/api/v1/beers')
-      .send({
+      .post('/api/v1/cubes')
+      .send({ 
         name: 'rubiks cube',
         dimensions: '3x3',
-        price: '$10'
+        price: '$12'
       });
     expect(res.body).toEqual({
       id: 1,
+      name: 'rubiks cube',
       dimensions: '3x3',
-      price: '$10'
+      price: '$12'
     });
   });
+
 });
+
